2fa628974cbf72de280f5a011a9828bea5a447bf
franchesoni <marchesoniacland@gmail.com>
2024-02-12 14:54:17 +0000

    new complete version (bad preprocessing)

--- solution.py
+++ solution.py
@@ -3,14 +3,13 @@ from typing import Tuple
 import numpy as np
 import scipy
 import matplotlib.pyplot as plt
-import torch
 import pandas as pd
 import os
+from skimage.morphology import skeletonize
+from cosines import inference
 
 
-def read_well(data_path, partition, i, return_first_frame_only):
-    data = pd.read_csv(os.path.join(data_path, "metaData.csv"))
-    data = data.loc[data["Partition"] == partition]
+def read_well(data, data_path, i, return_first_frame_only, return_labels=False):
 
     well_name = data.iloc[i]["Name_Well"]
     file_name = data.iloc[i]["Name_file"]
@@ -19,12 +18,28 @@ def read_well(data_path, partition, i, return_first_frame_only):
     labels_tie_list = [x.replace("'", "") for x in labels_tie_list]
 
     frames = []
+    if return_labels:
+        casings, ties = [], []
     for frame in tqdm.tqdm(labels_tie_list):
         name_frame = frame.split("_")[0]
-        image = prepare_data(name_frame, os.path.join(data_path, well_name, file_name))
+        if return_labels:
+            image, casing, tie = prepare_data_with_labels(
+                name_frame, os.path.join(data_path, well_name, file_name)
+            )
+        else:
+            image = prepare_data(
+                name_frame, os.path.join(data_path, well_name, file_name)
+            )
         if return_first_frame_only:
+            if return_labels:
+                return [image], [casing], [tie]
             return [image]
         frames.append(image)
+        if return_labels:
+            casings.append(casing)
+            ties.append(tie)
+    if return_labels:
+        return frames, casings, ties
     return frames
 
 
@@ -52,6 +67,26 @@ def prepare_data(name_frame: str, path: str) -> Tuple[np.ndarray, np.ndarray]:
         tdep = np.flipud(tdep)
 
     return image
+
+
+def prepare_data_with_labels(
+    name_frame: str, path: str
+) -> Tuple[np.ndarray, np.ndarray]:
+    casing_name = os.path.join(path, name_frame + "_casing_label.npy")
+    tie_name = os.path.join(path, name_frame + "_label.npy")
+    image = np.load(os.path.join(path, name_frame + ".npy"))
+    tdep = np.load(path + "/" + "TDEP.npy")
+    casing_original = np.load(casing_name).astype(np.int64)
+    tie = np.load(tie_name).astype(np.int64)
+
+    #  Sort the depths
+    if np.all((np.sort(tdep) == tdep)):
+        tdep = tdep
+    else:
+        image = np.flipud(image)
+        tdep = np.flipud(tdep)
+
+    return image, casing_original, tie
 
 
 def showimg(img, save=True, name=""):
@@ -98,8 +133,9 @@ def process(raw_img, vis=True, inv_width=40, var_thresh=1e-6):
         showimg(raw_img, name="S1_badrows")
 
     # remove first casing, assuming it's very high and very low
-    min_at = np.argmin(profile)  # where the min is
-    max_at = np.argmax(profile)  # where the max is
+    plen = profile.shape[0]  # assume casing is on the left half of the image
+    min_at = np.argmin(profile[: plen // 2])  # where the min is
+    max_at = np.argmax(profile[: plen // 2])  # where the max is
     if max_at < min_at:
         cut_at = min_at + (min_at - max_at) // 2  # use min and max to get a width param
     else:
@@ -122,7 +158,9 @@ def process(raw_img, vis=True, inv_width=40, var_thresh=1e-6):
         showimg(img, name="S4_abs")
 
     # gaussian blur on the horizontal dimension
-    img = scipy.ndimage.gaussian_filter1d(img, axis=1, sigma=img.shape[1] // inv_width)
+    sigma = img.shape[1] // inv_width
+    if sigma > 0:
+        img = scipy.ndimage.gaussian_filter1d(img, axis=1, sigma=sigma)
     if vis:
         showimg(img, name="S5_gauss")
     return img, cut_at, vars
@@ -139,30 +177,114 @@ def unprocess(img, cut_at, vars):
     return uimg
 
 
+def avg_displacement_error(paths, ties):
+    paths, ties = np.array(paths), np.array(ties)
+    A, R, T = paths.shape
+    assert paths.shape == ties.shape
+    pred_missing_rows, target_missing_rows = 0, 0
+    errors = []
+    for a in range(A):
+        tie_img = skeletonize(ties[a])
+        path_img = skeletonize(paths[a])
+        aerrors = []
+        for r in range(R):
+            line = tie_img[r]
+            linesum = line.sum()
+            if linesum == 0:
+                target_missing_rows += 1
+                continue  # skip if no ground truth
+            elif linesum > 2:
+                print(f"Warning! tie has too many targets at {a, r}!", end="\r")
+                offset = linesum // 2
+                continue
+            else:
+                offset = 0
+            pathline = path_img[r]
+            pathsum = pathline.sum()
+            if pathsum == 0:
+                pred_missing_rows += 1
+            pred = np.argmax(pathline)
+            target = np.argmax(line) + offset
+            displacement_error = np.abs(pred - target)
+            aerrors.append(displacement_error)
+        errors += aerrors
+
+    return np.mean(errors), pred_missing_rows, target_missing_rows
+
 
 def main(
-    # data_path="/HomeToo/data/curated/TIE",
-    # partition="train",
-    # i=0,
+    data_path="/HomeToo/data/curated/TIE",
+    partition="train",
+    i=0,
     no_vis=False,
+    skip_prediction=False,
 ):
     vis = not no_vis
 
-    # read image
-    # frames = read_well(data_path, partition, i, return_first_frame_only=True)
-    frames = np.array(np.load("frames.npy"))  # locally
-    # frames is [angles, depth, time]
-    uimgs = np.zeros_like(frames, dtype=float)
-    for find, raw_img in tqdm.tqdm(enumerate(frames)):
-        # visualize
-        if vis: showimg(raw_img, name="rawimg")
-        img, cut_at, vars = process(raw_img, vis=True)
-        if vis: showimg(img, name="processed")
-        uimg = unprocess(img, cut_at, vars)
-        uimgs[find] = uimg
+    print("reading metadata..." + " " * 40, end="\r")
+    data = pd.read_csv(os.path.join(data_path, "metaData.csv"))
+    data = data.loc[data["Partition"] == partition]
+    if not skip_prediction:
+        # make predictions
+        for i in range(len(data)):
+            print(f"working {i+1}/{len(data)}...")
+            print("reading..." + " " * 40, end="\r")
+            frames, casings, ties = read_well(
+                data, data_path, i, return_first_frame_only=False, return_labels=True
+            )
+            frames = np.array(frames)
+            # frames is [angles, depth, time]
+            print("preprocessing..." + " " * 40, end="\r")
+            uimgs = np.zeros_like(frames, dtype=float)
+            for find, raw_img in tqdm.tqdm(enumerate(frames)):
+                # visualize
+                if vis:
+                    showimg(raw_img, name="rawimg")
+                img, cut_at, vars = process(raw_img, vis=vis)
+                if vis:
+                    showimg(img, name="processed")
+                uimg = unprocess(img, cut_at, vars)
+                uimgs[find] = uimg
+            print("predicting..." + " " * 40, end="\r")
+            trr, paths = inference(uimgs)
+            print("saving..." + " " * 40, end="\r")
+            np.save(
+                f"everything_{partition}_{i}.npy",
+                {
+                    "trr": trr,
+                    "paths": paths,
+                    "frames": frames,
+                    "uimgs": uimgs,
+                    "casings": casings,
+                    "ties": ties,
+                },
+            )
 
-    np.save('uimgs.npy', uimgs)
-    
+    for i in range(len(data)):
+        print("-" * 20, i, "-" * 20)
+        print("loading..." + " " * 40, end="\r")
+        everything = np.load(
+            f"everything_{partition}_{i}.npy", allow_pickle=True
+        ).item()
+        # print('computing error...')
+        error, missing_preds, missing_targets = avg_displacement_error(
+            everything["paths"], everything["ties"]
+        )
+        showimg(everything["uimgs"][0], name=f"{i}_" + "uimgs0")
+        showimg(everything["uimgs"][17], name=f"{i}_" + "uimgs17")
+        showimg(everything["frames"][0], name=f"{i}_" + "frame0")
+        showimg(everything["frames"][17], name=f"{i}_" + "frame17")
+        showimg(everything["trr"][0], name=f"{i}_" + "trr0")
+        showimg(everything["trr"][17], name=f"{i}_" + "trr17")
+        showimg(everything["ties"][0], name=f"{i}_" + "ties0")
+        showimg(everything["ties"][17], name=f"{i}_" + "ties17")
+        showimg(everything["paths"][0], name=f"{i}_" + "paths0")
+        showimg(everything["paths"][17], name=f"{i}_" + "paths17")
+
+        print("average displacement =", error)
+        print("missing preds =", missing_preds)
+
+
 if __name__ == "__main__":
     from fire import Fire
 
